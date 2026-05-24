053cfeb5c708fc3a84d8144cd3d7ca8c452f6f55
Jozuan Martínez <jozuanlopez@gmail.com>
2026-05-23 18:39:08 -0600

    Removes projects that will not be part of the Clinic Queue System

--- library-shop-system/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿public class Program
-{
-    public static void Main()
-    {
-        var sistema = new SistemaBiblioteca();
-        sistema.Iniciar();
-    }
-}
--- medical-prescription/Receta.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FinalExamPro1;
-
-public class Receta
-{
-    public required string Nombre {get; set;}
-    public required int Dosis {get; set;}
-    public required string TipoDosis {get; set;}
-}
